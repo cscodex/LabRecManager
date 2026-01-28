4a8701cbd3c23c9162f0b74bb4cc10a780694a47
Preet Singh <Preetsingh921@gmail.com>
2026-01-28 18:18:55 +0530

    fix: enable trustHost for NextAuth

--- merit-entrance/src/lib/next-auth.ts
+++ merit-entrance/src/lib/next-auth.ts
@@ -12,6 +12,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    trustHost: true,
     pages: {
         signIn: '/',
         error: '/',
